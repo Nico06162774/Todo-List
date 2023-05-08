f3faa49d2bb71efa7b38c1d9b7060982cac36689
Nico06162774 <130480469+Nico06162774@users.noreply.github.com>
2023-05-08 21:37:33 +0800

    routes

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool =  new Pool({
     user: "postgres",
-    password: "6192774",
+    password: "61902774",
     host: "localhost",
     port: 5432,
     database: "perntodo"
