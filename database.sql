23d3fa09efde3afe868ffa718bd5e80fc4f0b2e2
Nico06162774 <130480469+Nico06162774@users.noreply.github.com>
2023-05-08 20:30:55 +0800

    connect

---
+++ database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE perntodo;
+
+CREATE TABLE todo(
+    todo_ID SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+    );
---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//routes//
+
+//create a todo
+app.post("/todos", async(req,res) =>{
+     try {
+        
+         
+
+
+     } catch (err) {
+        console.log(err.message);
+     }
+});
+//get all todo
+
+//get a todo
+
+//update a todo
+
+//delete a todo
+
+app.listen(5000,() =>{
+    console.log("Server connected to port 5000");
+});
